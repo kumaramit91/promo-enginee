5018acf8de811fb5d4ed6393865ac857202eb735
Admin <Admin@AMIT-COMPUTER.mshome.net>
2021-06-26 00:43:27 +0530

    JIRA-101: adding starter project

---
+++ proengine/src/main/java/com/am/proengine/ProengineApplication.java
@@ -0,0 +1,13 @@
+package com.am.proengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProengineApplication.class, args);
+	}
+
+}

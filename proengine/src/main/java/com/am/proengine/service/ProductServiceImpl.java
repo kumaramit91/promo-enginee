ca4f843796eb3c6aed3095efa573180d460149b7
Admin <Admin@AMIT-COMPUTER.mshome.net>
2021-06-26 11:30:33 +0530

    JIRA-119: updating service logic

--- proengine/src/main/java/com/am/proengine/service/ProductServiceImpl.java
+++ proengine/src/main/java/com/am/proengine/service/ProductServiceImpl.java
@@ -1,62 +1,72 @@
 package com.am.proengine.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.am.proengine.model.ActivePromotion;
 import com.am.proengine.model.CartDetails;
 import com.am.proengine.model.Product;
 
 @Service
 public class ProductServiceImpl implements ProductService{
 	
+	ActivePromotionService activePromotionService;
+	@Autowired
+	public ProductServiceImpl( ActivePromotionService activePromotionService) {
+		this.activePromotionService =activePromotionService;
+	}
+	
 	
 
 	@Override
 	public double getProductsTotalPrice(List<CartDetails> cartDetails) {
-		int counterOfA = 0;
-	    int priceOfA = 50;
-	    int counterOfB = 0;
-	    int priceOfB = 30;
-	    int counterOfC = 0;
-	    int priceOfC = 20;
-	    int counterOfD = 0;
-	    int priceOfD = 15;
-
-	    for (CartDetails cart:cartDetails)
-	    {
-	    
-	    	switch(cart.getSkuId()) {
-	    	case "A":
-            case "a":
-                counterOfA += 1;
-                break;
-            case "B":
-            case "b":
-                counterOfB += 1;
-                break;
-            case "C":
-            case "c":
-                counterOfC += 1;
-                break;
-            case "D":
-            case "d":
-                counterOfD += 1;
-                break;
-	    	}
-	    	}
-	    double totalPriceOfA = (counterOfA / 3) * 130 + (counterOfA % 3 * priceOfA);
-	    double totalPriceOfB = (counterOfB / 2) * 45 + (counterOfB % 2 * priceOfB);
-	    double totalPriceOfC = (counterOfC * priceOfC);
-	    double totalPriceOfD = (counterOfD * priceOfD);
-	    return totalPriceOfA + totalPriceOfB + totalPriceOfC + totalPriceOfD;
+		
+		//We have to fetch active promotion rule here from ActivePromotionServiceImpl Class
+	    Map<String, Double> productMap = getProducts().stream().collect(Collectors.toMap(Product::getSkuId,Product::getPrice));
+	    Map<String, Integer> cartMap =  cartDetails.stream().collect(Collectors.toMap(CartDetails::getSkuId,CartDetails::getCount));
+	    double totalPrice =0.0;
+	   if(cartMap.containsKey("A"))
+		   totalPrice += (cartMap.get("A") / 3) * 130 + (cartMap.get("A") % 3 * productMap.get("A"));
+	   if(cartMap.containsKey("B"))
+		   totalPrice += (cartMap.get("B") / 2) * 45 + (cartMap.get("B") % 2 * productMap.get("B"));
+	   if(cartMap.containsKey("C")&&!cartMap.containsKey("D"))
+		   totalPrice += cartMap.get("C") * productMap.get("C");
+	   if(cartMap.containsKey("D")&&!cartMap.containsKey("C"))
+		   totalPrice += cartMap.get("D")  * productMap.get("D");
+	   if(cartMap.containsKey("C")&&cartMap.containsKey("D")) {
+		   int ctrC = cartMap.get("C");
+		   int ctrD = cartMap.get("D");
+		   if(ctrC==ctrD) {
+			   totalPrice += ctrC * 30;
+		   }
+		   if(ctrC<ctrD) {
+			   totalPrice += ctrC * 30+(ctrD-ctrC)* productMap.get("D");
+		   }
+		   
+		   if(ctrC>ctrD) {
+			   totalPrice += ctrD * 30+(ctrC-ctrD)* productMap.get("C");
+		   }
+	   }
+		   
+	    return totalPrice;
 
 	}
 
 	@Override
 	public List<Product> getProducts() {
 		// TODO Auto-generated method stub
-		return null;
+		List<Product> products = new ArrayList<>();
+		products.add(new Product("A",50));
+		products.add(new Product("B",30));
+		products.add(new Product("C",20));
+		products.add(new Product("D",15));
+		
+		return products;
 	}
 
 }
